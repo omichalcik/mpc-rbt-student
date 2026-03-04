0f5d94f0bb0016a7f53be93e7d2fbbc0add01009
Stanislav Svědiroh <standa.svediroh@gmail.com>
2026-03-04 20:49:02 +0100

    update lab03 to use its own branch

--- src/localization_node.cpp
+++
@@ -1,8 +0,0 @@
-#include <mpc_rbt_solution/Localization.hpp>
-
-int main(int argc, char *argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<LocalizationNode>());
-    rclcpp::shutdown();
-    return 0;
-}
